207387fe2e2e39c17eab4bd951bebb5da4f20070
Jose Carlos <josecarlos231998@gmail.com>
2021-05-13 21:22:55 -0600

    Modal implemetado con animacion.

--- js/index.js
+++ js/index.js
@@ -2,45 +2,212 @@ var fondo = document.getElementById("fondo");
 let circulo1 = document.getElementById("circulo1");
 let circulo2 = document.getElementById("circulo2");
 let circulo3 = document.getElementById("circulo3");
+let circuloo1 = document.getElementById("circuloo1");
+let circuloo2 = document.getElementById("circuloo2");
+let circuloo3 = document.getElementById("circuloo3");
+// var h = 'h'
+let box = document.getElementById("boxN");
+let box2 = document.getElementById("boxN2");
 
-function cambiarFondo(x) {
-    if (x == 1) {
-        fondo.style.backgroundImage = "url(img/fondoPresentacion.jpg)";
-        circulo1.classList.add("circulitos2-seleccionado");
-        circulo2.classList.remove("circulitos2-seleccionado");
-        circulo3.classList.remove("circulitos2-seleccionado");
+var num = 0;
+function cambiarFondo(x, p, lado) {
+    console.log(p);
+
+    if (p == 'lado') {
+        if (lado == 'izq') {
+            num = num - 1;
+        }else{
+            num= num + 1 
+        }
+        if (num > 3) {
+            num = 3
+        } else if (num < 0){
+            num = 0
+        }
         
-    }else if(x == 2 ){
-        fondo.style.backgroundImage = "url(img/fondoPresentacion2.jpg)";
-        circulo2.classList.add("circulitos2-seleccionado");
-        circulo1.classList.remove("circulitos2-seleccionado");
-        circulo3.classList.remove("circulitos2-seleccionado");
-    }else{
-        fondo.style.backgroundImage = "url(img/fondoPresentacion4.jpg)";
-        circulo3.classList.add("circulitos2-seleccionado");
-        circulo1.classList.remove("circulitos2-seleccionado");
-        circulo2.classList.remove("circulitos2-seleccionado");
+        if (num == 0) {
+            fondo.style.backgroundImage = "url(img/slide1.jpg)";
+            circulo1.classList.add("circulitos2-seleccionado");
+            circulo2.classList.remove("circulitos2-seleccionado");
+            circulo3.classList.remove("circulitos2-seleccionado");
+        }else if(num == 1 ){
+            fondo.style.backgroundImage = "url(img/slide2.jpg)";
+            circulo2.classList.add("circulitos2-seleccionado");
+            circulo1.classList.remove("circulitos2-seleccionado");
+            circulo3.classList.remove("circulitos2-seleccionado");
+        }else if (num == 2){
+            fondo.style.backgroundImage = "url(img/slide3.jpg)";
+            circulo3.classList.add("circulitos2-seleccionado");
+            circulo1.classList.remove("circulitos2-seleccionado");
+            circulo2.classList.remove("circulitos2-seleccionado");
+        }
+    } else {
+            
+        if (p == 'h') {
+            
+            if (x == 1) {
+                fondo.style.backgroundImage = "url(img/slide1.jpg)";
+                circulo1.classList.add("circulitos2-seleccionado");
+                circulo2.classList.remove("circulitos2-seleccionado");
+                circulo3.classList.remove("circulitos2-seleccionado");
+                num = 0;
+                
+            }else if(x == 2 ){
+                num = 1;
+                fondo.style.backgroundImage = "url(img/slide2.jpg)";
+                circulo2.classList.add("circulitos2-seleccionado");
+                circulo1.classList.remove("circulitos2-seleccionado");
+                circulo3.classList.remove("circulitos2-seleccionado");
+            }else{
+                num = 2;
+                fondo.style.backgroundImage = "url(img/slide3.jpg)";
+                circulo3.classList.add("circulitos2-seleccionado");
+                circulo1.classList.remove("circulitos2-seleccionado");
+                circulo2.classList.remove("circulitos2-seleccionado");
+            }
+        }
+        else if (p == 'c'){
+            if (x == 1) {
+                fondo.style.backgroundImage = "url(img/slide1.jpg)";
+            }else if(x == 2 ){
+                fondo.style.backgroundImage = "url(img/slide2.jpg)";
+            }else{
+                fondo.style.backgroundImage = "url(img/slide3.jpg)";
+            }
+        }
+        else{
+            if (x == 1) {
+                fondo.style.backgroundImage = "url(img/slide1.jpg)";
+                circuloo1.classList.add("circulitos2-seleccionado");
+                circuloo2.classList.remove("circulitos2-seleccionado");
+                circuloo3.classList.remove("circulitos2-seleccionado");
+                
+            }else if(x == 2 ){
+                fondo.style.backgroundImage = "url(img/slide2.jpg)";
+                circuloo2.classList.add("circulitos2-seleccionado");
+                circuloo1.classList.remove("circulitos2-seleccionado");
+                circuloo3.classList.remove("circulitos2-seleccionado");
+            }else{
+                fondo.style.backgroundImage = "url(img/slide3.jpg)";
+                circuloo3.classList.add("circulitos2-seleccionado");
+                circuloo1.classList.remove("circulitos2-seleccionado");
+                circuloo2.classList.remove("circulitos2-seleccionado");
+            }
+        }
     }
 }
-
 
 // MENU FIJO
 let menuchico = document.getElementById('menufijo');
 let menu = document.getElementById('menu');
+let logofijo = document.getElementById("img-fijo2");
 let aaa = document.getElementById('imgg');
 let logo = document.getElementById('logofijo');
+let menu11= document.getElementById('menu11');
+let fijo= document.getElementById('fijoo');
 
 window.addEventListener('scroll', function(){
 if((window.pageYOffset) > 35){
     menu.classList.add('nav3');                                    
+    menu11.classList.add('cien');                                    
     aaa.classList.add('mg');                                    
     logo.classList.add('logo-fijo');      
     logo.style.display = "block";                              
+    logofijo.style.display = "none";                              
+    fijo.classList.add('fijo');                                    
 }
 else{
+    menu11.classList.remove('cien');                                    
+    fijo.classList.remove('fijo');                                    
+    menu11.classList.remove('nav-opcionesM');                              
+    logofijo.style.display = "block";                              
     menu.classList.remove('nav3');                                    
     aaa.classList.remove('mg');                                    
     logo.style.display = "none";                              
 }
 
 })   
+
+
+// document.querySelectorAll(".info-categorias img").forEach(el=>{
+    //     el.addEventListener("click", function(ev){
+        //         ev.stopPropagation();
+        //         this.parentNode.classList.add("active");
+        //     })
+        // });
+        
+        // document.querySelectorAll(".info-categorias").forEach(el=>{
+            //     el.addEventListener("click", function(ev){
+                //         this.classList.remove("active");
+                //     })
+                // })
+                
+                
+                
+    // function mostrarModal(a){
+    //     console.log(a);
+    //     la = 'modal'
+
+    //     let c1 = `modal${a}`
+    //     let c2 = `modal-info${a}`
+    //     let c3 = `icono${a}`
+    //     let c4 = `modal-img${a}`
+    //     let c5 = `modal-texto${a}`
+    //     console.log(c1);
+    //     console.log(c2);
+            
+    //     let modal1 = document.getElementById(c1);
+    //     let modal2 = document.getElementById(c2);
+    //     let modal3 = document.getElementById(c3);
+    //     let modal4 = document.getElementById(c4);
+    //     let modal5 = document.getElementById(c5);
+    //     console.log(modal2);
+      
+    //     modal1.classList.add('modall');                                    
+    //     modal2.classList.add('info-modal');                                    
+    //     modal3.classList.add('modal-img');                                    
+    //     modal4.classList.add('modal-texto');                                    
+    //     modal5.classList.add('vericono'); 
+    // }
+
+    function mostrarModal(a,b,c,d,e){
+
+        console.log(a);
+        console.log(b);
+        console.log(c);
+        console.log(d);
+        console.log(e);
+        // console.log(f);
+
+        let modal1 = document.getElementById(a);
+        let modal2 = document.getElementById(b);
+        let modal3 = document.getElementById(c);
+        let modal4 = document.getElementById(d);
+        let modal5 = document.getElementById(e);
+        // let modal5 = document.getElementById(f);
+        console.log(modal1);
+        
+        modal1.classList.add('ver');                                    
+        modal1.classList.add('modall');                                    
+        modal2.classList.add('info-modal');                                    
+        modal3.classList.add('modal-img');                                    
+        modal4.classList.add('modal-texto');                                    
+        modal5.classList.add('vericono');                                    
+        // modal5.classList.add('titul');                                    
+    }
+
+    function cerrarModal(a,b,c,d,e){
+        let modal1 = document.getElementById(a);
+        let modal2 = document.getElementById(b);
+        let modal3 = document.getElementById(c);
+        let modal4 = document.getElementById(d);
+        let modal5 = document.getElementById(e);
+
+        modal1.classList.remove('ver');                                    
+        modal1.classList.remove('modall');                                    
+        modal2.classList.remove('info-modal');                                    
+        modal3.classList.remove('modal-img');                                    
+        modal4.classList.remove('modal-texto');                                    
+        modal5.classList.remove('vericono');
+
+    }
